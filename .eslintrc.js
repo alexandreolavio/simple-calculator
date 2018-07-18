467b176c1837f6db0a61c84f01bcafd76482fe65
alexandre.olavio <alexandre.olavio@m2msolutions.com.br>
2018-07-17 21:08:03 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "extends": "airbnb",
+  "plugins": [
+      "react",
+      "jsx-a11y",
+      "import"
+  ],
+  "env": {
+    "browser": true
+  },
+  "rules": {
+    "import/no-extraneous-dependencies": 0,
+    "class-methods-use-this": 0,
+    "no-useless-constructor": 0,
+    "no-unused-expressions": 0,
+    "no-underscore-dangle": 0,
+    "no-param-reassign": 0,
+    "no-eval": 0,
+  }
+};
